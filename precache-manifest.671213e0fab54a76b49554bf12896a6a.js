8ae131bda7208495c7391c4982f27fb0a19f7025
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-10-20 13:19:41 +0300

    Updates

---
+++ precache-manifest.671213e0fab54a76b49554bf12896a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24167488edcffa791b91",
+    "url": "/GithubFinder/static/css/main.78e6fd49.chunk.css"
+  },
+  {
+    "revision": "24167488edcffa791b91",
+    "url": "/GithubFinder/static/js/main.7345de60.chunk.js"
+  },
+  {
+    "revision": "b3da8e90ce4eb57d6028",
+    "url": "/GithubFinder/static/js/runtime~main.098c7ace.js"
+  },
+  {
+    "revision": "1cba83327f9f52b2af32",
+    "url": "/GithubFinder/static/js/2.4621cc27.chunk.js"
+  },
+  {
+    "revision": "af6f9364134017b7f51f4db087682862",
+    "url": "/GithubFinder/index.html"
+  }
+];
